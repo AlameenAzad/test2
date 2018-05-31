78da165e89afb03a330ef008d6103753bc1c1166
alameen <alameen.azad@protonmail.com>
2018-06-01 01:37:24 +0300

    latest b4 email

--- js/main.js
+++ js/main.js
@@ -20,45 +20,5 @@ backabout.onclick = function () {
     nav.classList.toggle("nav-active");
 }
 
-//$( "#tip" ).submit(function( e ) {
-//    e.preventDefault();
-//    $.ajax({
-//        url: tip.php,
-//        type:'POST',
-//        data:
-//        {
-//            tip_email: $('#tip_email').val(),
-//            ad_id: $('#ad_id').val()
-//        },
-//        success: function(msg)
-//        {
-//
-//            alert('Email Sent');
-//        }
-//    });
-//});
-//function submit() {
-//    var name = document.getElementById("name").value;
-//    var email = document.getElementById("email").value;
-//    var msg = document.getElementById("msg").value;
-//
-//    var dataString = 'Email from:' + name + 'Email:' + email + '&password1=' + password + '&contact1=' + contact;
-//
-//        // AJAX code to submit form.
-//        $.ajax({
-//            type: "POST",
-//            url: "http://alameen.rf.gd/mail.php",
-//            data: {
-//                name : name,
-//                email: email,
-//                msg: msg
-//            },
-//            cache: false,
-//            success: function (html) {
-//                alert("Email submitted to php");
-//            }
-//        });
-//    }
-//    return false;
-//}
+
 
