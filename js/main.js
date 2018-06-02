e2cc24e65cbcc4ea5272fefd2b1cfb9baaf4aaee
alameen <alameen.azad@protonmail.com>
2018-06-02 18:45:53 +0300

    j

--- js/main.js
+++ js/main.js
@@ -37,17 +37,54 @@ return false;
 }
 
 function SubForm(e){
-    e.preventDefault();
-    var url="https://untitled-2pxai0iw14ew.runkit.sh/",
-        data=form.serialize();
-    $.ajax({
-        url:url,
-        type:'get',
-        data:data,
-        success:function(result){
-           //whatever you wanna do after the form is successfully submitted
-            alert(result);
-           }
-        });
+    var url="https://untitled-2pxai0iw14ew.runkit.sh/" + form.serialize();
+    console.log(url);
+    var ret = postAsync(url) ;
+    // hint: encodeURIComponent()
+
+if (ret.match(/^XHR error/)) {
+    console.log(ret);
+    return;
+    }
+//    e.preventDefault();
+//    var url="https://untitled-2pxai0iw14ew.runkit.sh/",
+//        data=form.serialize();
+//    $.ajax({
+//        url:url,
+//        type:'get',
+//        data:data,
+//        success:function(result){
+//           //whatever you wanna do after the form is successfully submitted
+//            alert(result);
+//           }
+//        });
 //    return false;
     }
+function postAsync(url2get)    {
+    var req;
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    if (req != undefined) {
+        // req.overrideMimeType("application/json"); // if request result is JSON
+        try {
+            req.open("GET", url2get, false); // 3rd param is whether "async"
+            }
+        catch(err) {
+            alert("couldnt complete request. Is JS enabled for that domain?\\n\\n" + err.message);
+            return false;
+            }
+        req.send(); // param string only used for POST
+
+        if (req.readyState == 4) { // only if req is "loaded"
+            if (req.status == 200)  // only if "OK"
+                { return req.responseText ; }
+            else    { return "XHR error: " + req.status +" "+req.statusText; }
+            }
+        }
+    alert("req for getAsync is undefined");
+}
+
+//var var_str = "var1=" + var1  + "&var2=" + var2;
